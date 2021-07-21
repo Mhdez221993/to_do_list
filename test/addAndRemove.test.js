e6a968c378945e318d0d948332e37f39bb90e2fa
Moises <mhdezcoronado@gmail.com>
2021-07-21 20:30:56 +0200

    Remove unnessessary code

--- test/addAndRemove.test.js
+++
@@ -1,7 +0,0 @@
-const newList = require('../src/addAndRemove');
-
-describe('removew', () => {
-    test('remove and element from the list', () => {
-        expect(newList.addIndex()).toBe(1)
-    })
-})
