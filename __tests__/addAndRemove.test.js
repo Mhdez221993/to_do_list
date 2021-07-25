02bf4cc06980938458481b693a74b1224fbfaef0
Moises <mhdezcoronado@gmail.com>
2021-07-25 16:25:57 +0200

    Remove test file

--- __tests__/addAndRemove.test.js
+++
@@ -1,8 +0,0 @@
-// import { test } from "@jest/globals";
-import { expect } from "@jest/globals";
-import { TaskList } from "../src/addAndRemove";
-
-test('shud return the leght', () => {
-  let newtask = new TaskList()
-  expect(newtask.add(4)).toBe(8)
-})
